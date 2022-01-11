5b1be1f9185a349b76d6cf1a8d9a7fc7fad36e28
ramki45 <89958455+ramki45@users.noreply.github.com>
2022-01-11 18:28:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+let customer = [];
+let rooms = [];
+let remove = [];
+
+
+//post room details
+app.post('/roomdetail',function(req,res){
+    rooms.push(req.body);
+    
+    res.json({
+        message: 'room details'
+    })
+    })
+
+//get room details
+app.get('/rooms',function(req,res){
+   
+    res.json(rooms);
+
+})
+
+//post customer detail with bookings
+app.post('/customer_detail',function(req,res){
+    req.body.id = customer.length + 1;
+    customer.push(req.body)
+    res.json({
+        message: 'customer added'
+    })
+   
+})
+
+//get customer room with booking status
+app.get('/customers',function(req,res){
+    customer.push(req.body)
+    res.json(customer)
+})
+
+
+//customer with booked data
+app.get('/booking_customer',function(req,res){
+    customer.push(req.body)
+    const output = customer.map(({status, ...remove})=> remove)
+    res.json(output)
+        
+
+})
+
+
+
+app.listen(3001);
